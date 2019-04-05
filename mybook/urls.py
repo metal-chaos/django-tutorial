4c951937c1a0f9eab045b5842a5786f69c667c59
metal-chaos <nightrage.metal66@gmail.com>
2019-04-05 21:17:20 +0900

    added each templates cms/base.html and cms/book_list.html

--- mybook/urls.py
+++ mybook/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include   # ←, includeを追加
 
 urlpatterns = [
-    path('cms/', include('cms.urls')),
+    path('cms/', include('cms.urls')),   # ←ここを追加
     path('admin/', admin.site.urls),
 ]
